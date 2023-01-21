4a54b7f704e2616ae8a82776851f9ccceffcb9bd
SwathisriSuravarapu <119840037+SwathisriSuravarapu@users.noreply.github.com>
2023-01-21 20:39:08 +0530

    #include<stdio.h> int main() {     int n,m,i,c,b,d;     scanf("%d%d",&m,&n);     for(i=m+1;i<n;i++)     {        c=i*i;        d=i*i*i;        printf("%d ",i);        printf("%d ",c);        printf("%d ",d);
        }
        
    }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,c,b,d;
+    scanf("%d%d",&m,&n);
+    for(i=m+1;i<n;i++)
+    {
+       c=i*i;
+       d=i*i*i;
+       printf("%d ",i);
+       printf("%d ",c);
+       printf("%d
+",d);
+       
+       
+    }
+    
+}
