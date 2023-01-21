2f9bc9483074aa56232c23e439cb52de97fa0b10
SwathisriSuravarapu <119840037+SwathisriSuravarapu@users.noreply.github.com>
2023-01-21 20:35:43 +0530

    #include<stdio.h> int main() {     int i,n,s=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         {             s=s+i;         }     }     if(n==s)     {         printf("PERFECT");     }     else if(s<n)     {         printf("DEFICIENT");     }     else if(s>n)     {         printf("ABUNDANT");     } }

---
+++ PDA.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,s=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            s=s+i;
+        }
+    }
+    if(n==s)
+    {
+        printf("PERFECT");
+    }
+    else if(s<n)
+    {
+        printf("DEFICIENT");
+    }
+    else if(s>n)
+    {
+        printf("ABUNDANT");
+    }
+}
